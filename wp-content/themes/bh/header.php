a66945d5d3b30d49fa87b430c8b5aa7e92c8d15d
Nir Goldberg <nir@htmline.com>
2015-07-15 16:03:51 +0300

    1. Add Google Merchant Center verification code 2. Add Facebook Remarketing Pixel script 3. Add Facebook Conversion Pixel script 4. Plugins update
    Former-commit-id: c515ab74529d00220ec26aea17a9c546a3a4e4c4

--- wp-content/themes/bh/header.php
+++ wp-content/themes/bh/header.php
@@ -14,17 +14,14 @@
 			<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
 		<![endif]-->
 		
-		<script>
-			var _prum = [['id', '54febcb3abe53d756b1004a5'],
-				['mark', 'firstbyte', (new Date()).getTime()]];
-			(function() {
-				var s = document.getElementsByTagName('script')[0],
-					p = document.createElement('script');
-				p.async = 'async';
-				p.src = '//rum-static.pingdom.net/prum.min.js';
-				s.parentNode.insertBefore(p, s);
-			})();
-		</script>
+		<?php
+			get_template_part('views/header/pingdom');
+			get_template_part('views/header/facebook-remarketing');
+
+			if ( is_wc_endpoint_url('order-received') ) {
+				get_template_part('views/header/facebook-conversion');
+			}
+		?>
 		
 	</head>
 	
---
+++ wp-content/themes/bh/views/header/facebook-conversion.php
@@ -0,0 +1,17 @@
+<!-- Facebook Conversion Code for Checkouts - Beit Hatfutsot 1 -->
+<script>(function() {
+var _fbq = window._fbq || (window._fbq = []);
+if (!_fbq.loaded) {
+var fbds = document.createElement('script');
+fbds.async = true;
+fbds.src = '//connect.facebook.net/en_US/fbds.js';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(fbds, s);
+_fbq.loaded = true;
+}
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', '6025740659065', {'value':'0.00','currency':'USD'}]);
+</script>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6025740659065&amp;cd[value]=0.00&amp;cd[currency]=USD&amp;noscript=1" /></noscript>
+<!-- End Facebook Conversion Code -->
