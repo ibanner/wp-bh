3ebfe1a9142956c31aacf2f9d449e68189d21323
Nir Goldberg <nir@htmline.com>
2015-10-28 11:54:58 +0200

    1. Modify robots.txt in order to prevent duplications in blog archive 2. Change Facebook Pixel scripts 3. Modify Google Shopping Feed in order to include product excerpt as well as product content in the description field
    Former-commit-id: c092ae9491fbfd82fb0191a3bb45f9e2e836e22b

--- wp-content/themes/bh/views/header/facebook-conversion.php
+++ wp-content/themes/bh/views/header/facebook-conversion.php
@@ -1,4 +1,4 @@
-<!-- Facebook Conversion Code for Checkouts - Beit Hatfutsot 1 -->
+<!-- Facebook Conversion Code for Checkouts -->
 <script>(function() {
 var _fbq = window._fbq || (window._fbq = []);
 if (!_fbq.loaded) {
@@ -11,7 +11,7 @@ _fbq.loaded = true;
 }
 })();
 window._fbq = window._fbq || [];
-//window._fbq.push(['track', '6025740659065', {'value':'0.00','currency':'USD'}]);	// push conversion value and currency in Checkout Thankyou endpoint
+//window._fbq.push(['track', '6030843605312', {'value':'0.00','currency':'ILS'}]);	// push conversion value and currency in Checkout Thankyou endpoint
 </script>
-<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6025740659065&amp;cd[value]=0.00&amp;cd[currency]=USD&amp;noscript=1" /></noscript>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6030843605312&amp;cd[value]=0.00&amp;cd[currency]=ILS&amp;noscript=1" /></noscript>
 <!-- End Facebook Conversion Code -->
