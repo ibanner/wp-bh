229752b872de632f1fb64475c39e4ad63bd8132a
Nir Goldberg <nir@htmline.com>
2015-11-12 13:26:09 +0200

    Shop homepage new design - banners
    Former-commit-id: ad523a72cfe74d8fd79ab2a2246360c973c10898

--- wp-content/themes/bh/views/woocommerce/archive/archive-product-home.php
+++ wp-content/themes/bh/views/woocommerce/archive/archive-product-home.php
@@ -9,23 +9,12 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
 
-<div class="container">
-	<?php
-		/**
-		 * woocommerce_breadcrumb
-		 */
-		woocommerce_breadcrumb();
-	?>
-</div>
-
-<div class="container">
-	<?php
-		/**
-		 * BH_shop_home hook
-		 *
-		 * @hooked	BH_shop_home_banners - 10
-		 * @hooked	BH_shop_home_product_sliders - 20
-		 */
-		do_action('BH_shop_home');
-	?>
-</div>
+<?php
+	/**
+	 * BH_shop_home hook
+	 *
+	 * @hooked	BH_shop_home_banners - 10
+	 * @hooked	BH_shop_home_product_sliders - 20
+	 */
+	do_action('BH_shop_home');
+?>
