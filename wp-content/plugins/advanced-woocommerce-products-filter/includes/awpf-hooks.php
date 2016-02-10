b3d44ab222a2d8b7a628f0e294dd9bde1ad5d870
Nir Goldberg <nir@htmline.com>
2016-02-11 01:21:53 +0200

    Advanced WooCommerce Products Filter plugin - create widget frontend class
    Former-commit-id: 51133d294da5e32b174b09800c8c7ef9b4082bd0

--- wp-content/plugins/advanced-woocommerce-products-filter/includes/awpf-hooks.php
+++ wp-content/plugins/advanced-woocommerce-products-filter/includes/awpf-hooks.php
@@ -9,9 +9,3 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-/**
- * awpf_before_main_content
- *
- * @see		awpf_output_widget_title
- */
-add_action( 'awpf_before_main_content', 'awpf_output_widget_title', 10, 1 );
