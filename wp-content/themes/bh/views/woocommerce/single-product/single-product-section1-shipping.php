8de28557aea4b30707f89885b56823b8aa26affe
Nir Goldberg <nir@htmline.com>
2016-03-22 15:29:24 +0200

    Shop product page - product shipping info
    Former-commit-id: 8d197feced1bd683a7ff1c836e5244fc5ed62af1

---
+++ wp-content/themes/bh/views/woocommerce/single-product/single-product-section1-shipping.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Single Product shipping info
+ *
+ * @author 		Beit Hatfutsot
+ * @package 	bh/views/woocommerce/single-product
+ * @version     1.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+?>
+
+<div class="single-product-meta-shipping product-meta-section">
+
+	<?php // title ?>
+	<div class="product-meta-section-title"><?php _e('Shipment info', 'BH'); ?></div>
+
+	<?php // content ?>
+	<div class="shipping-info"><?php _e('International shipment is available, shipping cost is based on shipping location and product dimensions', 'BH'); ?></div>
+
+</div>
