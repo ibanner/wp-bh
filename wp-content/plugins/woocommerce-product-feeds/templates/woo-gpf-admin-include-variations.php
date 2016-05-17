4644aa43c2c64de5abaacdef00464cc46645415c
Nir Goldberg <nir@htmline.com>
2016-05-17 13:14:16 +0300

    1. WordPress version upgrade (4.5.2) 2. Plugins version upgrade
    Former-commit-id: d83c422d4e3480edb6bde7635b4962664f666928

--- wp-content/plugins/woocommerce-product-feeds/templates/woo-gpf-admin-include-variations.php
+++ wp-content/plugins/woocommerce-product-feeds/templates/woo-gpf-admin-include-variations.php
@@ -10,6 +10,6 @@
 <p>
 	<input type="checkbox" class="woocommerce_gpf_field_selector" name="woocommerce_gpf_config[include_variations]" id="woocommerce_gpf_config[include_variations]" {include_variations_selected}>
 	<label for="woocommerce_gpf_config[include_variations]"><?php
-	echo __( 'Include variations in your feed.)', 'woocommerce_gpf' ); // WPCS: XSS OK.
+	echo __( 'Include variations in your feed.', 'woocommerce_gpf' ); // WPCS: XSS OK.
 	?></label>
 </p>
