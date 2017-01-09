d7a9a1ac5c69839905a1cec676082803db218355
Nir Goldberg <nir@htmline.com>
2017-01-09 12:39:50 +0200

    Main events slider - change management method to manually add event slides

--- wp-content/themes/bh/views/main/layout-slider.php
+++ wp-content/themes/bh/views/main/layout-slider.php
@@ -1,48 +1,49 @@
 <?php
+/**
+ * Main - Slider layout
+ *
+ * @author 		Beit Hatfutsot
+ * @package 	bh/views/main
+ * @version     1.0
+ */
 
-	global $events;
-	
-	// get future events
-	$args = array(
-		'post_type'			=> 'event',
-		'posts_per_page'	=> -1,
-		'no_found_rows'		=> true,
-		'orderby'			=> 'menu_order',
-		'order'				=> (ICL_LANGUAGE_CODE == 'he') ? 'DESC' : 'ASC',
-		'meta_query'		=> array(
-			'relation'		=> 'AND',
-			array(
-				'key'		=> 'acf-event_homepage_slider_indicator',
-				'value'		=> true
-			),
-			array(
-				'key'		=> 'acf-event_end_date',
-				'value'		=> date_i18n('Ymd'),
-				'type'		=> 'DATE',
-				'compare'	=> '>='
-			)
-		)
-	);
-	
-	if ( function_exists('BH_get_cached_wp_query') ) :
-		$events = BH_get_cached_wp_query($args, 'events-' . ICL_LANGUAGE_CODE);
-	else :
-		$events = array();
-		$query = new WP_Query($args);
-		
-		if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();
-			$events[] = $post;
-		endwhile; endif; wp_reset_postdata();
-	endif;
-	
-	if ($events) :
-	
-		// build event elements
-		get_template_part('views/main/slider/set-event-elements');
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-		// display events slider
-		get_template_part('views/main/slider/display-events-slider');
-		
-	endif;
+// check for ACF existence
+if ( ! function_exists(get_field) )
+	return;
+
+// layout parameters
+$slider = get_field('acf-options_exhibitions_and_events_slider', 'option');
+
+if ( ! $slider )
+	return;
+
+global $events;
+$events = array();
+
+$wpml_lang = function_exists('icl_object_id') ? ICL_LANGUAGE_CODE : '';
+
+// get slider events
+foreach ($slider as $s) {
+	$event = $s['event'];
+
+	if ( $event ) {
+		if ( $wpml_lang == 'he' ) {
+			array_unshift($events, $event);
+		}
+		else {
+			$events[] = $event;
+		}
+	}
+}
+
+if ($events) {
+
+	// build event elements
+	get_template_part('views/main/slider/set-event-elements');
+
+	// display events slider
+	get_template_part('views/main/slider/display-events-slider');
 	
-?>
+}
